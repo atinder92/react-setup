2b2aa937cd1eac721ebf838eb7df728897ed0486
Atinder Singh <atinder.s.92@gmail.com>
2017-07-06 16:33:18 +1000

    Code Updates

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,6 @@ import ReactDOM from 'react-dom';
 import HelloWorld from './react/helloworld.component.js'
 
 
-console.log("App Loaded");
-
-var person = new Person("Atinder",25);
-person.displayInformation();
 
 //Render React Component
 ReactDOM.render(<HelloWorld/>,document.getElementById("reactOutput"));
